ca7af5682d3fecd53c3bf2c3809a9d5398de427d
Tim_Lt <timlin@ltedu.com.tw>
2023-12-06 17:31:53 +0800

    ready to step 6

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import vue from "@astrojs/vue";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [vue(), tailwind()]
+});
---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,12 @@
+import type { APIContext } from 'astro'
+import rss, { pagesGlobToRssItems } from '@astrojs/rss'
+
+export async function GET(context: APIContext) {
+  return rss({
+    title: 'Astro Learner | Blog',
+    description: 'My journey learning Astro',
+    site: context.site || '',
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>zh-Hant-TW</language>`,
+  })
+}
