58fbb3cf1be2dca5935a16047f1f8ebcd02cf347
Tim_Lt <timlin@ltedu.com.tw>
2023-12-07 14:04:25 +0800

    feat(finished): finished tutorial and add Vue component

---
+++ src/components/Greeting.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps<{
+  messages: string[]
+}>()
+
+function randomMessage() {
+  return props.messages[Math.floor(Math.random() * props.messages.length)]
+}
+
+const greeting = ref<string>(randomMessage())
+const setGreeting = () => (greeting.value = randomMessage())
+</script>
+
+<template>
+  <div>
+    <h3>{{ greeting }}! Thank you for visiting!</h3>
+    <button
+      type="button"
+      @click="setGreeting"
+    >
+      New Greeting
+    </button>
+  </div>
+</template>
+
+<style scope></style>
